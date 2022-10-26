1f9bade65bd0dca78ce1c3504b71811a0d0f497c
Evaristo <evamorero1@gmail.com>
2022-10-25 21:03:18 -0300

    desafio01

--- archivos js/index.js
+++ archivos js/index.js
@@ -3,22 +3,29 @@ class usuario {
 constructor(nombre, apellido, libros, mascotas){
  this.nombre = nombre
  this.apellido = apellido
- this.libros = libros
-this.mascotas = mascotas
+ this.libros = [libros]
+this.mascotas = [mascotas]
 }
 
-getFullName() {
-    return this.nombre + " " + this.apellido
+getFullName () {
+return (`mi nombre completo es ${this.nombre} ${this.apellido}`)
 }
 
+addMascotas(){
+this.mascotas.push("loro")
+}
 
-getBookName(){
+getBookNames(){
     return this.libros
 }
 
+countMascotas(){
+   return this.mascotas.length
 }
+
+}
+
 let usuario1 = new usuario ("juan", "perez", ["pinocho", "lazarillo"],["perro", "gato"]);
 
 
-
-console.log(usuario1.getBookName())
+console.log()
